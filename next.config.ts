71b3a0469a86b511606b215730d375f19d7d9c59
윤지은 <yoonzeen@natecorp.com>
2026-03-27 11:13:04 +0900

    [MOD] build test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "nate-tv-short-videos";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: isGithubPages ? "export" : undefined,
+  trailingSlash: isGithubPages,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repoName}` : undefined,
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
