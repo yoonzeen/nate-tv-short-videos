8c61f4b2a2c883718f900fff27a6e0835110a4ba
윤지은 <yoonzeen@natecorp.com>
2026-04-01 09:19:24 +0900

    [MOD] update video data

--- data/videos.ts
+++ data/videos.ts
@@ -12,20 +12,20 @@ export const videos: VideoItem[] = [
     title: "저 개그맨 아들 있어요🤹‍♂️ 신개념 자식 플러팅 시전 한 오지헌 아빠🤵❣",
     channel: "TV CHOSUN",
     description: "조선의 사랑꾼 114회",
-    src: "https://nate-tvchosun-h.smartmediarep.com/smc/nate/multi/eng/CS1_710807/2f747663686f73756e2f4353332f323032365f707265766965772f656e7465722f433230323230303139345f636c69702f433230323230303139345f636c69705f32303236303332335f313134725f31375f7433332e6d7034/0-0-0/content.mp4?solexpire=1774959229&solpathlen=215&soltoken=1cdc61f5e8b23d5867fc5384f59b800a&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=50a14c34-7cdd-4352-acca-1db67e173bdc&itemtypeid=33",
+    src: "https://nate-tvchosun-h.smartmediarep.com/smc/nate/multi/eng/CS1_710807/2f747663686f73756e2f4353332f323032365f707265766965772f656e7465722f433230323230303139345f636c69702f433230323230303139345f636c69705f32303236303332335f313134725f31375f7433332e6d7034/0-0-0/content.mp4?solexpire=1775045913&solpathlen=215&soltoken=91e454da0be449ddca51edd928e8f361&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=51b49c15-b745-40fd-98ba-679ed2b659f8&itemtypeid=33",
   },
   {
     id: "5459744",
     title: "찐사랑 찾고 싶어 솔로녀들에게 온도차 느껴지는 돌직구 던지는 20기 영식!",
     channel: "SBS PLUS X ENA",
     description: "사랑은 계속된다 EP.158",
-    src: "https://nate-sbs-h.smartmediarep.com/smc/nate/multi/eng/S03_22000628176/2f7362732f6e6574766d70342f75706c6f61642f3232302f30303632382f32323030303632383137366b5f7433332e6d7034/0-0-0/content.mp4?solexpire=1774959248&solpathlen=142&soltoken=2d069fed3a65d769d517772d0744b60c&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=e1b256ba-e197-4509-b7d2-25b21fa5a3ae&itemtypeid=33",
+    src: "https://nate-sbs-h.smartmediarep.com/smc/nate/multi/eng/S03_22000628176/2f7362732f6e6574766d70342f75706c6f61642f3232302f30303632382f32323030303632383137366b5f7433332e6d7034/0-0-0/content.mp4?solexpire=1775045931&solpathlen=142&soltoken=de2eeb28cc6961336af994f84e58a02d&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=ec4a9181-24d6-432a-9569-a1228c0de914&itemtypeid=33",
   },
   {
     id: "5459713",
     title: "[하이라이트] 하루 만 원으로 버티는 남편… 그러나 빚까지 내며 과소비하는 현실 MZ 아내💳",
     channel: "JTBC",
     description: "이혼숙려캠프",
-    src: "https://nate-jtbc-h.smartmediarep.com/smc/nate/multi/eng/J01_VO10912177/2f6a7462632f435030303030303030312f70726f672f656e7465722f6469766f72636563616d70322f323032362f30332f342f564f31303931323137373232303631363739345f7433332e6d7034/0-0-0/content.mp4?solexpire=1774959276&solpathlen=197&soltoken=7ffe039f32921fc4503c6049adc4b2ef&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=5dc90216-eec2-41e5-9a7a-6eabf02b2f7a&itemtypeid=33",
+    src: "https://nate-jtbc-h.smartmediarep.com/smc/nate/multi/eng/J01_VO10912177/2f6a7462632f435030303030303030312f70726f672f656e7465722f6469766f72636563616d70322f323032362f30332f342f564f31303931323137373232303631363739345f7433332e6d7034/0-0-0/content.mp4?solexpire=1775045943&solpathlen=197&soltoken=be9a8b9cffccdb73f4a985adf9b07e16&soltokenrule=c29sZXhwaXJlfHNvbHBhdGhsZW58c29sdXVpZA==&soluriver=2&soluuid=fc86ed33-ed42-4e67-96a4-bd2b6460747f&itemtypeid=33",
   },
 ];
