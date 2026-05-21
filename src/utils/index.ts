7a1c3234d8b7eca5ad1085a5e4434d783874fdda
윤지은 <yoonzeen@natecorp.com>
2026-05-21 10:10:29 +0900

    [MOD] 통계코드 확인

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,25 +4,26 @@
  */
 const domainStat = 'm_ndr.nate.com/nateon/nate';
 const servicecode = 'mw01';
+export function getCookie(key: string) {
+  let result = null;
+  if (typeof document === 'undefined') return;
+  const cookie = document.cookie.split(';');
+
+  cookie.forEach((item) => {
+    item = item.replace(/ /g, '');
+
+    const dictionary = item.split('=');
+    if (key === dictionary[0]) {
+      result = dictionary.slice(1).join('=');
+    }
+  });
+  return result;
+}
 export const sendPV = () => {
     const image = new Image();
-    image.src = `//stat.nate.com/stat/mstat.tiff?cp_url=[${domainStat}??ndrparam1=${servicecode}&ndrparam2=&ndrparam3=&ndrparam4=&ndrbr=&ndrparam6=&ndrparam9=&ndru3=${getCookie('ndrn')}]&t=${getTimestamp()}`;
+    console.log(getCookie('UD3'));
+    image.src = `//stat.nate.com/stat/mstat.tiff?cp_url=[${domainStat}??ndrparam1=${servicecode}&ndrparam2=&ndrparam3=&ndrparam4=&ndrbr=&ndrparam6=&ndrparam9=&ndru3=${getCookie('UD3')}]&t=${getTimestamp()}`;
 };
-export function getCookie(key: string) {
-    let result = null;
-    if (typeof document === 'undefined') return;
-    const cookie = document.cookie.split(';');
-  
-    cookie.forEach((item) => {
-      item = item.replace(/ /g, '');
-  
-      const dictionary = item.split('=');
-      if (key === dictionary[0]) {
-        result = dictionary.slice(1).join('=');
-      }
-    });
-    return result;
-}
 function getTimestamp() {
 const now = new Date();
 // 기존 t 파라미터 형식 맞춤: 0514150513572 → MMDDHHmmssSSS
